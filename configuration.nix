fc65bea204911ff9fd277914e1a8a9bfda5017e0
Elliot <elliot.gibeaux@proton.me>
2025-11-24 22:55:29 +0100

    used nixos main config for laptop

--- configuration.nix
+++ configuration.nix
@@ -1,37 +1,26 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, inputs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-	];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  services = { 
-	blueman.enable = true;
+  security.rtkit.enable = true;
 
-	openssh.enable = true;
-
-	displayManager.sddm = { 
-	  enable = true;
-	  wayland.enable = true;
+  hardware = {
+  	bluetooth = { 
+  	  enable = true; 
+	  powerOnBoot = true;
+	  settings = {
+	  	General = {
+		  Experimental = true;
+		  FastConnectable = true;
+	    };
+	  	Policy = { 
+		  AutoEnable = true;
+	    };
+	  };
     };
-
-  	xserver = { 
-	  videoDrivers = [ "nvidia" "modesetting" ];
-	  xkb = {
-        layout = "us";
-	   	variant = "";
-      };
-	};
-  };
-
-  hardware = { 
-	bluetooth.enable = true;
-	graphics.enable = true;
     nvidia = {
 	  open = true;
   	  prime = {
@@ -43,70 +32,77 @@
         nvidiaBusId = "PCI:1:0:0";
 	  };
 	};
+
+	firmware = [
+	  pkgs.linux-firmware
+      pkgs.sof-firmware
+    ];
   };
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernel =  { 
+	  sysctl = {
+        "net.ipv6.conf.all.disable_ipv6" = true;
+        "net.ipv6.conf.default.disable_ipv6" = true;
+		"vm.max_map_count" = 16777216; # star citizen stuff
+		"fs.file-max" = 524288; # same here
+      };
+	};
+  };
+
+  virtualisation.docker.enable = true;
 
   programs = {
-  	bat.enable = true;
-	starship.enable = true;
-	hyprland.enable = true;
-  	fish = {
-  	  enable = true; 
-	  interactiveShellInit = "set fish_greeting";
-	  shellAbbrs = {
-	    up = "sudo nixos-rebuild switch --flake ~/.config/flake-nixos";
+    starship.enable = true;
+    firefox.enable = true;
+    bat.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+    fish = {
+      enable = true;
+      interactiveShellInit = "set fish_greeting";
+      shellAbbrs = { 
+	  	up = "sudo nixos-rebuild switch --flake ~/.config/flake-nixos";
+		nclean = "sudo nix-collect-garbage --delete-older-than 7d";
 	  };
-	  shellAliases = {
-	    ls = "eza --icons";
+      shellAliases = { 
+	  	ls = "eza --icons"; 
+		icat = "kitten icat";
 	  };
-	};
-	neovim = {
-	  enable = true;
-      package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
-	};
-	waybar.enable = true;
-	steam = {
-	  enable = true;
-	  remotePlay.openFirewall = true;
-	  dedicatedServer.openFirewall = true;
-	};
-	git = {
-	  enable = true;
-	  config = {
-		user = {
-	  	  email = "elliot.gibeaux@proton.me";
-		  name = "Elliot";
-		};
-		code.core = "nvim";
-	  };
-	};
-  };
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Bootloader.
-  boot.loader =  { 
-  	systemd-boot.enable = true; 
-  	efi.canTouchEfiVariables = true;
+    };
+    neovim = {
+      enable = true;
+	  viAlias = true;
+	  vimAlias = true;
+	  defaultEditor = true;
+      package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default; # to remove on the 25.12
+    };
   };
 
   networking = {
-	networkmanager.enable = true;
-	hostName = "nixos"; 
+    networkmanager.enable = true;
+    hostName = "nixos";
+    enableIPv6 = false;
   };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  nix.settings = { 
+  	experimental-features = [ "nix-command" "flakes" ];
+	substituters = ["https://nix-citizen.cachix.org"];
+    trusted-public-keys = ["nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="];
+  };
 
-
-  # Enable networking
-
-  # Set your time zone.
   time.timeZone = "Europe/Paris";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_FR.UTF-8";
     LC_IDENTIFICATION = "fr_FR.UTF-8";
@@ -119,9 +115,101 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
-  # Configure keymap in X11
+  services = {
+    xserver = {
+      xkb.layout = "us";
+    };
+    displayManager.gdm.enable = true;
+	desktopManager.gnome.enable = true;
+	gnome = {
+	  core-apps.enable = false;
+	  core-developer-tools.enable = false;
+	  games.enable = false;
+	};
+	flatpak = { 
+	  enable = true;
+	  remotes = [
+	  	{ name = "RSILauncher"; location = "https://mactan-sc.github.io/rsilauncher/RSILauncher.flatpakrepo"; }
+		{ name = "flathub"; location = "https://dl.flathub.org/repo/flathub.flatpakrepo"; }
+	  ];
+	  packages = [
+		{ appId = "io.github.mactan_sc.RSILauncher"; origin = "RSILauncher"; }
+		"ch.tlaun.TL"
+		"ca.desrt.dconf-editor"
+	  ];
+	};
+  };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  home-manager = {
+	useGlobalPkgs = true;
+	useUserPackages = true;
+
+	users.elliot = 
+	  { pkgs, config, ...}:
+	  {
+		home = {
+		  stateVersion = "25.05";
+
+		  packages = with pkgs; [
+			gnomeExtensions.dash-to-dock
+			gnomeExtensions.blur-my-shell
+			gnomeExtensions.search-light
+		  ];
+		};
+
+		dconf = { 
+		  enable = true;
+		  settings = {
+		    "org/gnome/shell" = {
+			  disable-user-extensions = false;
+			  enabled-extensions = [
+				"blur-my-shell@aunetx"
+			    "dash-to-dock@micxgx.gmail.com"
+				"search-light@icedman.github.com"
+			  ];
+		    };
+		  	"org/gnome/desktop/input-sources" = {
+			  xkb-options = [ "ctrl:nocaps" ];
+		    };
+			"org/gnome/shell/extensions/dash-to-dock" = {
+			  dash-max-icon-size = 44;
+			  custom-theme-shrink = true;
+			  apply-custom-theme = false;
+			  intellihide-mode = "ALL_WINDOWS";
+			};
+			"org/gnome/desktop/wm/preferences" = {
+			  button-layout = "appmenu:minimize,close";
+			  resize-with-right-button = true;
+			};
+			"org/gnome/desktop/applications/terminal" = {
+			  exec = "ghostty";
+			};
+			"org/gnome/shell/keybindings" = {
+			 show-screen-recording-ui = "['<Shift><Super>s']";
+			};
+			"org/gnome/mutter" = {
+			  experimental-features = "['scale-monitor-framebuffer']";
+			};
+		  };
+		};
+		programs = {
+		  ghostty.enable = true;
+		  git = {
+			enable = true;
+			settings = {
+			  user = {
+				name = "Elliot";
+				email = "elliot.gibeaux@proton.me";
+			  };
+			  core = { 
+			    editor = "nvim";
+			  };
+			};
+		  };
+		};
+	  };
+  };
+
   users.users.elliot = {
     shell = pkgs.fish;
     isNormalUser = true;
@@ -129,55 +217,59 @@
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
-  # Enable automatic login for the user.
-  #services.getty.autologinUser = "elliot";
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
 
-  environment.systemPackages = with pkgs; [
-	 gcc
-	 clang
-     kitty
-     firefox
-     btop
-     hyprpaper
-	 wget
-	 hyprlock
-	 discord
-	 clang-tools
-	 nixd
-	 bluez
-	 albert
-	 fastfetch
-	 eza
-  ];
+  nixpkgs.config.allowUnfree = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  environment = { 
+  	sessionVariables = {
+		TERMINAL = "ghostty";
+	};
+  	systemPackages = with pkgs; [
+	  # wine
+	  wine
+	  wine64
+	  winetricks
+  
+	  # gui apps
+      mpv
+	  feishin
+	  bottles
+      discord
+      firefox
+      nautilus
+      obs-studio
+      qbittorrent
+  	  gnome-tweaks
+      protonvpn-gui
+	  gnome-calendar
+	  gnome-calculator
+	  gnome-text-editor
+  
+	  # compiler and such
+	  go
+	  zig
+      lua
+      gcc
+      clang
+  
+	  # languages servers
+	  zls
+      nixd
+	  gopls
+      clang-tools
+      lua-language-server
+      docker-language-server
+  
+	  # cli and tui tools
+      eza
+      wget
+      btop
+      ripunzip
+      fastfetch
+      wl-clipboard
+  	];
+  };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 }
--- flake.nix
+++ flake.nix
@@ -1,14 +1,23 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	nix-flatpak.url = "github:gmodena/nix-flatpak";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+	
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
-  outputs = { self, nixpkgs, neovim-nightly-overlay, ... }@inputs: {
+  outputs = { self, nix-flatpak, nixpkgs, neovim-nightly-overlay, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
-      modules = [ ./configuration.nix ];
+      modules = [ 
+	    ./configuration.nix 
+		home-manager.nixosModules.home-manager
+		nix-flatpak.nixosModules.nix-flatpak
+	  ];
     };
   };
 }
